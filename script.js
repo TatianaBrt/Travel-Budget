1ef64150e17ad9a8b844b7c400b642e5f4488ce5
Glitch (glitch-hello-website) <none>
2022-07-31 08:24:17 +0000

    🏙🕋 Checkpoint ./index.html:43031560/6134 ./script.js:43031560/2519 ./style.css:43031560/6626

--- script.js
+++ script.js
@@ -1,27 +1,45 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and moves a button you can add from the README
-*/
+const button=document.querySelector("#btn");
+button.addEventListener("click",calculateAmount);
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
+const buttonTip=document.querySelector("#btnTip");
+buttonTip.addEventListener("click",showTip);
+function showTip(e){
+    e.preventDefault();
+    tip.style.display="inline";
+    }
+    const buttonTipRest=document.querySelector("#btnTipRest");
+    buttonTipRest.addEventListener("click",showTipRest);
+function showTipRest(e){
+    e.preventDefault();
+    tiprest.style.display="inline";
+    }
 
-/* 
-Make the "Click me!" button move when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'dipped' code in style.css
-    btn.classList.toggle("dipped");
-  };
+function calculateAmount(e){
+    e.preventDefault();
+    const hotels=document.querySelector("#hotels").value;
+    const Restaurants=document.querySelector("#Restaurants").value;
+    const plane=document.querySelector("#plane").value;
+    const food=document.querySelector("#food").value;
+    const entert=document.querySelector("#entert").value;
+    const transport=document.querySelector("#transport").value;
+    const museum=document.querySelector("#museum").value;
+    const tip=document.querySelector("#tip").value;
+    const tiprest=document.querySelector("#tiprest").value;
+
+    let tipHotels=hotels*tip
+    let tipRestoran=Restaurants*tiprest
+    let totalHotels=hotels;
+    let totalRestor=Restaurants;
+    let ttotalPlane=plane;
+    let totalFood=food;
+    let totalEntert=entert;
+    let totalTransport=transport;
+    let totalMuseum=museum;
+    let totalSum=+tipHotels+ +tipRestoran+ +totalHotels+ +totalRestor+ +ttotalPlane+ +totalFood+ +totalEntert+ +totalTransport+ +totalMuseum;
+     
+    totalSum=totalSum.toFixed(2);
+    document.querySelector("#totalNumber").textContent=totalSum;
+
+   
+    
 }
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
